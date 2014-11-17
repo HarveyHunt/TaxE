5a7b525a7ff671067142386199a9fae9ce83317b
OwenNash <on268@hotmail.co.uk>
2014-11-17 16:32:30 +0000

    x

--- core/src/com/taxe/ExampleObject.java
+++ core/src/com/taxe/ExampleObject.java
@@ -18,7 +18,7 @@ public class ExampleObject {
     public ExampleObject(final GameCore game){
         this.game = game;
         // Init the object
-        tex = new Texture("image.png");
+        tex = new Texture("badlogic.jpg");
         x = 100;
     }
 
