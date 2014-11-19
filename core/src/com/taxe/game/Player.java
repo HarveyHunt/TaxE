caa01713754a5f3df37e0fa6513cf63caf574b13
Vladimir <vl.daglis@gmail.com>
2014-11-19 16:06:17 +0000

    Adding a pyle of new classes

---
+++ core/src/com/taxe/game/Player.java
@@ -0,0 +1,46 @@
+package com.taxe.game;
+
+import com.taxe.game.resources.Fuel;
+import com.taxe.game.resources.Gold;
+import com.taxe.game.trains.Train;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Vladimir on 19/11/2014.
+ */
+public class Player {
+
+    private final ArrayList<Train> trains;
+    private final Gold gold;
+    private final Fuel fuel;
+    private final Homebase homebase;
+
+    public Player(Homebase homebase, ArrayList<Train> trains, Gold gold, Fuel fuel) {
+        this.homebase = homebase;
+        this.trains = trains;
+        this.gold = gold;
+        this.fuel = fuel;
+    }
+
+    public ArrayList<Train> getTrains() {
+        return trains;
+    }
+
+    public Gold getGold() {
+        return gold;
+    }
+
+    public Homebase getHomebase() {
+        return homebase;
+    }
+
+    public Fuel getFuel() {
+        return fuel;
+    }
+
+    // Just an example to think about
+    public void doStuff(Player p) {
+        p.getFuel().setUsedFuel(10);
+    }
+}
---
+++ core/src/com/taxe/game/trains/TrainType.java
@@ -0,0 +1,12 @@
+package com.taxe.game.trains;
+
+/**
+ * Created by Vladimir on 19/11/2014.
+ */
+public class TrainType {
+
+    /*TO DO
+    Here go different attributes of the train, like speed, fuelUsage etc.
+     */
+
+}
