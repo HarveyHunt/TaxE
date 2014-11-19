8c64fbb55402aa2e1e3a1eecad8ec5854d0c66d0
OwenNash <on268@hotmail.co.uk>
2014-11-19 20:38:18 +0000

    City class early stages

--- core/src/com/taxe/game/City.java
+++ core/src/com/taxe/game/City.java
@@ -1,7 +1,21 @@
 package com.taxe.game;
 
+import java.util.ArrayList;
+
 /**
  * Created by Owen on 19/11/2014.
  */
 public class City {
+
+    private Coordinate position;
+    private ArrayList <Task> tasks;
+
+    public City(int x, int y){
+
+    }
+
+    public City(){
+        this(0, 0);
+    }
+
 }
