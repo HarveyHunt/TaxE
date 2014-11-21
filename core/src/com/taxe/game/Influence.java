2c357abd865dc85c303e76c581a734bdf820c294
asthasaxena <as1770@york.ac.uk>
2014-11-21 14:01:56 +0000

    Merge branch 'master' of https://github.com/OwenNash/TaxE
    Conflicts:
    	core/src/resources/Thumbs.db

--- core/src/com/taxe/game/Influence.java
+++ core/src/com/taxe/game/Influence.java
@@ -4,33 +4,43 @@ package com.taxe.game;
  * Created by Owen on 19/11/2014.
  */
 public class Influence {
-    private int player1Influence;
-    private int player2Influence;
+    private int totalInfluence;
+    private int[] players;
 
-    public Influence(int player1Influence, int player2Influence) {
-        this.player1Influence = player1Influence;
-        this.player2Influence = player2Influence;
+    public Influence(int totalInfluence, int player1Influence, int player2Influence) {
+        this.totalInfluence = totalInfluence;
+        players = new int[2];
+        this.players[0] = player1Influence;
+        this.players[1] = player2Influence;
+    }
+
+    public void awardInfluence(int player, int quantity) {
+        players[player - 1] += quantity;
+        int newTotal = players[0] + players[1];
+        if (newTotal > totalInfluence){
+            players[2 - player] -= newTotal - totalInfluence;
+        }
     }
 
     public int getPlayer1Influence() {
-        return player1Influence;
+        return players[0];
     }
 
     public void setPlayer1Influence(int player1) {
-        this.player1Influence = player1;
+        this.players[0] = player1;
     }
 
     public int getPlayer2Influence() {
-        return player2Influence;
+        return players[1];
     }
 
     public void setPlayer2Influence(int player2) {
-        this.player2Influence = player2;
+        this.players[1] = player2;
     }
 
-    public int totalInfluence(int player1, int player2){
-        return player1 + player2;
+    public int getTotalInfluence(){
+        return totalInfluence;
     }
-    
+
 }
 
