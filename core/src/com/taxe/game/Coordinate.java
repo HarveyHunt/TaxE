c471aa88f1e453f457c865baf78988a27a56fb51
asthasaxena <as1770@york.ac.uk>
2015-01-08 17:07:41 +0000

    Merge branch 'master' of https://github.com/OwenNash/TaxE
    Conflicts:
    	core/src/resources/Thumbs.db

--- core/src/com/taxe/game/Coordinate.java
+++ core/src/com/taxe/game/Coordinate.java
@@ -49,9 +49,9 @@ public final class Coordinate {
     public static Coordinate coordinateAlongLine(Coordinate cA, Coordinate cB, double percentage) {
         // Returns the point a certain percentage along a line between nodes 1 and 2
         // percentage as a decimal between 0 and 1.
-        double dX = cB.getX() - cA.getX();
-        double dY = cB.getY() - cA.getY();
-        return new Coordinate(cA.getX() + dX * percentage, cA.getY() + dY * percentage);
+        double dX = cB.x - cA.x;
+        double dY = cB.y - cA.y;
+        return new Coordinate(cA.x + dX * percentage, cA.y + dY * percentage);
     }
 
     public static double angleBetween(Coordinate cA, Coordinate cB) {
