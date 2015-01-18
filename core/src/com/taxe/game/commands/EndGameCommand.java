c20ce6ac3f68038212c6f3326d095000f249f8ec
OwenNash <on268@hotmail.co.uk>
2015-01-18 11:41:26 +0000

    I think this works. Yay

--- core/src/com/taxe/game/commands/EndGameCommand.java
+++ core/src/com/taxe/game/commands/EndGameCommand.java
@@ -10,9 +10,9 @@ public class EndGameCommand implements Commandable {
     public void executeCommand(GameCore game, Object target) {
         int winner;
         if (game.getActivePlayer() == game.getPlayers().get(0)) {
-            winner = 0;
-        } else {
             winner = 1;
+        } else {
+            winner = 0;
         }
         game.getGui().createGameEndMenu(winner);
         game.getGui().getHUD().lockButtons();
