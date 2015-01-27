a2482b1e8bab743f53eaec7b13813b7119232e70
Peter Bryant <petermbryant@gmail.com>
2015-01-27 15:40:06 +0000

    Added absolute goal completion checking

--- core/src/com/taxe/Main.java
+++ core/src/com/taxe/Main.java
@@ -11,7 +11,7 @@ import com.taxe.menu.MenuScreen;
 public class Main extends Game {
 
     public SpriteBatch batch;
-    private GameCore game;
+    private static GameCore game;
     private MenuScreen menuScreen;
 
     @Override
@@ -33,6 +33,8 @@ public class Main extends Game {
         setScreen(menuScreen);
         game.dispose();
     }
+
+    public static GameCore getGameCore() { return game; }
 
     @Override
     public void render() {
--- core/src/com/taxe/game/tasks/Task.java
+++ core/src/com/taxe/game/tasks/Task.java
@@ -1,5 +1,14 @@
 package com.taxe.game.tasks;
 
+import com.taxe.game.cargo.Cargo;
+import com.taxe.game.nodes.City;
+import com.taxe.game.GameCore;
+import com.taxe.game.player.Player;
+import com.taxe.game.trains.Train;
+import com.taxe.Main;
+
+import static com.taxe.Main.*;
+
 /**
  * Top-level class for representing tasks. Different types of tasks are implemented by extending this class.
  * <p>
@@ -8,18 +17,26 @@ package com.taxe.game.tasks;
  */
 public abstract class Task {
     private final String name, objective;
+    private City endCity;
+    private Cargo cargoType;
     private int taskTime;
+
 
     /**
      * Creates an instance of Task with given name, objective and time during which it can be completed.
      *
-     * @param name      name of a task.
-     * @param objective objective of a task.
-     * @param taskTime  time when tasks is available for completion.
+     * @param name      Name of a task.
+     * @param objective String describing task
+     * @param endCity   The ending city of the task
+     * @param cargoType The type of cargo to be delivered
+     * @param taskTime  time when tasks is available for completion
      */
-    public Task(String name, String objective, int taskTime) {
+
+    public Task(String name, String objective, City endCity, Cargo cargoType, int taskTime) {
         this.name = name;
         this.objective = objective;
+        this.endCity = endCity;
+        this.cargoType = cargoType;
         this.taskTime = taskTime;
     }
 
@@ -42,6 +59,20 @@ public abstract class Task {
     }
 
     /**
+     * Returns end city of the task.
+     *
+     * @return end city of task.
+     */
+    public City getEndCity() { return endCity; }
+
+    /**
+     * Returns the type of cargo to be transported.
+     *
+     * @return type of cargo to be transported.
+     */
+    public Cargo getCargoType() { return cargoType; }
+
+    /**
      * Returns remaining time when task can be completed.
      *
      * @return time when tasks is available for completion.
@@ -57,6 +88,24 @@ public abstract class Task {
      */
     public void setTasktime(int taskTime) {
         this.taskTime = taskTime;
+    }
+
+    /**
+     * Checks whether the goal has been completed
+     *
+     * @return Boolean value according to whether the task has been completed.
+     */
+    public boolean isComplete() {
+        for (Player player: Main.getGameCore().getPlayers()) {
+            for (Train train : player.getTrains()) {
+                if (train.getNode() == this.getEndCity()) {
+                    return train.getCargo() == this.getCargoType();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 }
 
