30dc3f323eb9e35e47dd7f7b4c5692f13946cb1e
hwjj500 <hwjj500@york.ac.uk>
2015-02-09 18:12:33 +0000

    Making progress with Cards

---
+++ core/src/com/taxe/game/cards/Block.java
@@ -0,0 +1,7 @@
+package com.taxe.game.cards;
+
+/**
+ * Created by henry on 09/02/15.
+ */
+public class Block {
+}
---
+++ core/src/com/taxe/game/commands/BlockCityCommand.java
@@ -0,0 +1,7 @@
+package com.taxe.game.commands;
+
+/**
+ * Created by henry on 09/02/15.
+ */
+public class BlockCityCommand {
+}
