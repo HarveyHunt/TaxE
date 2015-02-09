898fb4a73d678809e63cbecfeab8b0f632d500cd
FlinnBurgess <flinn.burgess@gmail.com>
2015-02-09 22:14:24 +0000

    Added a new calculation method to DealDamageCommand. Damage is now based of off influence.

--- core/src/com/taxe/game/commands/DealDamageCommand.java
+++ core/src/com/taxe/game/commands/DealDamageCommand.java
@@ -1,6 +1,7 @@
 package com.taxe.game.commands;
 
 import com.taxe.game.GameCore;
+import com.taxe.game.nodes.City;
 import com.taxe.game.nodes.Homebase;
 import com.taxe.game.player.Player;
 
@@ -22,8 +23,16 @@ public class DealDamageCommand implements Commandable {
 
         // Deal damage to player's homebase
         Player p = (Player) target;
+        float influenceSum = 0;
+        for(Player p2 : game.getPlayers()) {
+            if(!(p.equals(p2))) {
+                for(City c : game.getMap().getCities()) {
+                    influenceSum += c.getInfluence(p2);
+                }
+            }
+        }
         Homebase h = p.getHomebase();
-        h.changeHealthBy(-100);
+        h.changeHealthBy(Math.round(-100 * influenceSum));
         game.getGui().getHud().getHealthbar(p).setPercentage((float) h.getHealth() / h.getMaxHealth());
 
         // If homebase is destroyed, end game
