119864cba91045a16ead6079705dbd7af587dd44
hwjj500 <hwjj500@york.ac.uk>
2015-02-09 16:27:00 +0000

    Fixed a git issue

--- core/src/com/taxe/game/cards/Card.java
+++ core/src/com/taxe/game/cards/Card.java
@@ -1,7 +1,11 @@
 package com.taxe.game.cards;
 
+import com.taxe.game.GameCore;
+
 /**
  * Created by henry on 09/02/15.
+ * Specifies methods that all cards must implement.
  */
-public class Card {
+public interface Card {
+    public void activateCard(GameCore game);
 }
--- core/src/com/taxe/game/commands/RewardCardCommand.java
+++ core/src/com/taxe/game/commands/RewardCardCommand.java
@@ -1,7 +1,30 @@
 package com.taxe.game.commands;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.taxe.game.GameCore;
+import com.taxe.game.nodes.City;
+import com.taxe.game.player.Player;
+import com.taxe.game.tasks.Task;
+import com.taxe.game.trains.Train;
+import com.taxe.game.trains.TrainStates;
+import com.taxe.game.util.Coordinate;
+
 /**
  * Created by henry on 09/02/15.
+ * Reward the player specified in the target argument with a randomly selected card
  */
-public class RewardCardCommand {
+public class RewardCardCommand implements Commandable{
+    @Override
+    public void executeCommand(GameCore game, Object target) throws IllegalArgumentException {
+
+        if (!(target instanceof Player)) {
+            throw new IllegalArgumentException("Target not instance of Player");
+        }
+
+        // Randomly select a card
+    }
 }
