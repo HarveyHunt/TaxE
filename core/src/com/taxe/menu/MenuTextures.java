71c0aea1228d0cc2dc06b879ceec409d5305ada8
Harvey Hunt <harveyhuntnexus@gmail.com>
2015-02-11 15:45:46 +0000

    Renamed Gui and Menu texture folders

--- core/src/com/taxe/menu/MenuTextures.java
+++ core/src/com/taxe/menu/MenuTextures.java
@@ -7,10 +7,9 @@ import com.badlogic.gdx.graphics.Texture;
  */
 public abstract class MenuTextures {
 
-    public static final Texture START_GAME_BUTTON = new Texture("Menu/start-game-button.png");
-    public static final Texture OPTIONS_BUTTON = new Texture("Menu/options-button.png");
-    public static final Texture EXIT_GAME_BUTTON = new Texture("Menu/exit-game-button.png");
-
-    public static final Texture START_SCREEN = new Texture("Menu/start-screen.png");
+    public static final Texture START_GAME_BUTTON = new Texture("menu/start-game-button.png");
+    public static final Texture OPTIONS_BUTTON = new Texture("menu/options-button.png");
+    public static final Texture EXIT_GAME_BUTTON = new Texture("menu/exit-game-button.png");
+    public static final Texture START_SCREEN = new Texture("menu/start-screen.png");
 
 }
