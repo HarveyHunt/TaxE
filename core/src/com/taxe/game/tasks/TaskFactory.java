b68378c54075971e6305a43d574693adfd19ac22
Harvey Hunt <harveyhuntnexus@gmail.com>
2015-02-12 13:19:07 +0000

    Don't generate goals for cities that already have goals heading to them.

--- core/src/com/taxe/game/tasks/TaskFactory.java
+++ core/src/com/taxe/game/tasks/TaskFactory.java
@@ -19,11 +19,27 @@ public class TaskFactory {
     public Task generateTask() {
         List<City> cities = this.game.getMap().getCities();
         Random rgen = new Random();
-        City newEnd = cities.get(rgen.nextInt(cities.size() - 1));
+        City newEnd;
+
+        do
+            newEnd = cities.get(rgen.nextInt(cities.size() - 1));
+        while (!unusedCity(newEnd));
 
         if (rgen.nextInt(2) == 0)
             return new Task(newEnd, new Wheat((rgen.nextInt(100))), rgen.nextInt(5) + 1);
         else
             return new Task(newEnd, new Coal(rgen.nextInt(100)), rgen.nextInt(5) + 1);
+    }
+
+    /**
+     * Discover if a city is already the end point of a task, if so return false.
+     * @param  The city to be checked for.
+     * @return true if the city is unused, false otherwise.
+     */
+    private boolean unusedCity(City c) {
+        for (Task t : game.getTasks())
+            if (t.getEndCity() == c)
+                return false;
+        return true;
     }
 }
